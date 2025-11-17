6965bda491fe3589fe5b6eccbda1b4bb76591bcc
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-11-17 14:28:55 +0800

    [+] Material 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.svg'
+	import "@fontsource/roboto"
+	import "./app.sass"
+	import "./material.scss"
+    import { AppBar } from "m3-svelte"
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
 <svelte:head>
