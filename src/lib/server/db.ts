9ef6268eb76aeb0e6fa295253606927e6eb226d1
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-11-18 13:51:53 +0800

    [F] Fix auth

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb"
 
-export const mongo = new MongoClient(process.env.MONGO_URL || "mongodb://cat:meow@localhost:27017/kashi")
+export const mongo = new MongoClient(process.env.MONGO_URL || "mongodb://cat:meow@localhost:27017/")
 mongo.connect()
 export const db = mongo.db()
 
