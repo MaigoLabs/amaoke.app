ce480f39f78eaf008f5582013bdde18b84fe6e59
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-11-21 11:28:26 +0800

    [+] Appbar button for import

--- src/routes/playlists/[category]/+page.svelte
+++ src/routes/playlists/[category]/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-  import AppBar from "../../../components/appbar/AppBar.svelte";
-import ImageListItem from "../../../components/listitem/ImageListItem.svelte";
+  import { goto } from "$app/navigation"
+  import AppBar from "../../../components/appbar/AppBar.svelte"
+  import ImageListItem from "../../../components/listitem/ImageListItem.svelte"
+  import MenuItem from "../../../components/material3/MenuItem.svelte"
   import type { PageProps } from "./$types"
 
   let { data }: PageProps = $props()
 </script>
 
-<AppBar title={data.isMine ? '我的歌单' : '推荐歌单'} right={[
-  // TODO
-  {icon: "i-material-symbols:more-vert", onclick: () => alert('More clicked')}
-]} />
+<AppBar title={data.isMine ? '我的歌单' : '推荐歌单'}>
+  <MenuItem icon="i-material-symbols:download" onclick={() => goto("/import/netease")}>从网易云导入</MenuItem>
+</AppBar>
 
 <div class="vbox p-content gap-12px">
   {#each data.playlists as pl}
