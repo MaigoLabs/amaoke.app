1c2086ac422fb4abb479d7515ffc2f571830c03c
Courier <artar2004@gmail.com>
2025-11-22 23:11:48 +0400

    404 page init

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,23 +1,20 @@
 <script>
-  import AppBar from "$lib/ui/appbar/AppBar.svelte" 
-  import Button from "$lib/ui/Button.svelte"
+  import AppBar from "$lib/ui/appbar/AppBar.svelte";
+  import Button from "$lib/ui/Button.svelte";
   import { getI18n } from "$lib/i18n";
+  import { goto } from '$app/navigation';
 
   const t = getI18n().errorPage;
 </script>
 
-<svelte:head>
-  <title>404 — Page Not Found</title>
-</svelte:head>
-
 <main> 
-  
-<AppBar title={t.title} >
-</AppBar>
+  <AppBar title={t.title} />
   <div class="p-content error-page__content">
     <p class="error-page__text" >{@html t.message}</p>
     <div class="error-page__button-container">
-      <Button class="error-page__button">{t.return}</Button>
+      <a href="/">
+        <Button class="error-page__button">{t.return}</Button>
+      </a>
     </div>
   </div>
 </main>
